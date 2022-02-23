1340f21317ccafe4284553f5d59e3e28e8e8f761
LizRadds <avilradish@gmail.com>
2022-02-23 12:24:39 +0100

    init: start human-readable-number-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function toReadable (number) {
-  
+    const numbersSimple = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    const numbersDecadeSimple = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const numbersDecade = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty','seventy' , 'eighty' , 'ninety']
+    const numbersHundred = ['', 'one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred']
+    let arr = number.toString().split
+    let str = String(number)
+    if (str.length === 1){
+        
+    }
 }
